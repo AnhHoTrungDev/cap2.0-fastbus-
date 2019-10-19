8c15d881f11b88d68e5ce0a5e8c759f739ca51b2
Hồ Trung Anh <hotrunganhht1912@gmail.com>
2019-10-19 16:29:47 +0700

    ok
    ok

--- Sources/FastBus/src/controller/LoadHomePageServlet.java
+++ Sources/FastBus/src/controller/LoadHomePageServlet.java
@@ -40,7 +40,7 @@ public class LoadHomePageServlet extends HttpServlet {
 		List<DiaDiem> listPlace=dd.getListPlaceBO();
 		
 		request.setAttribute("listPlace", listPlace);
-		RequestDispatcher rd=request.getRequestDispatcher("/Views/users/homepage.jsp");
+		RequestDispatcher rd=request.getRequestDispatcher("/Views/users/homePage.jsp");
 		rd.forward(request, response);
 	}
 
