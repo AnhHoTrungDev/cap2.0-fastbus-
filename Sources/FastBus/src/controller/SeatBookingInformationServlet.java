e95427751dd81364c01d32e6e474e43d50f14aa9
quocle28297 <quocle28292@gmail.com>
2019-11-24 15:59:21 +0700

    fixx_mail_2

--- Sources/FastBus/src/controller/SeatBookingInformationServlet.java
+++ Sources/FastBus/src/controller/SeatBookingInformationServlet.java
@@ -132,7 +132,7 @@ public class SeatBookingInformationServlet extends HttpServlet {
 			}
 
 			if (new SeatBookingBO().insertSeatBO(listSeat) == 1) {
-				SendMailCustomerDAO(tripInfor, user, listSeat);
+				new SendEmailBO().SendMailCustomerBO(tripInfor, user, listSeat);
 				url = "LoadHomePageServlet";
 			} else {
 				url = "Views/users/busGarageDetail.jsp";
@@ -154,44 +154,6 @@ public class SeatBookingInformationServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
-
-	public void SendMailCustomerDAO(ChuyenXe tripInfor, User user, List<SeatBooking> listSeat) {
-
-		// 1) get the session object
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.host", MailConfig.HOST_NAME);
-		props.put("mail.smtp.socketFactory.port", MailConfig.SSL_PORT);
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.port", MailConfig.SSL_PORT);
-
-		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(MailConfig.APP_EMAIL, MailConfig.APP_PASSWORD);
-			}
-		});
-		// 2) compose message
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(MailConfig.APP_EMAIL));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
-
-			// 3) create HTML content
-			message.setSubject("[FastBus] Xác nhận thông tin vé xe ngay " + tripInfor.getStartDate() + ". ","UTF-8");
-			String htmlContent = " <p>Thông tin các vé xe bạn đã đặt: <p>" 
-								+ " <p>Nhà xe: " + user.getName() + "</p>"
-								+ " <p> Chuyến xe: " + tripInfor.getStartPlace() + " - " + tripInfor.getEndPlace() + "</p>"
-								+ " <p>Ngày đi: " + tripInfor.getStartDate() + "</p>" + " <p>Giờ đi: " + tripInfor.getStartTime()
-								+ "</p>" + " <p>Ngày đi: " + tripInfor.getStartDate() + "</p>";
-			message.setContent(htmlContent,"text/html; charset=UTF-8");
-			// 4) send message
-			Transport.send(message);
-
-			System.out.println("Message sent successfully");
-		} catch (MessagingException ex) {
-			ex.printStackTrace();
-		}
 	}
 
 }
--- Sources/FastBus/src/model/dao/SendEmaiDAO.java
+++ Sources/FastBus/src/model/dao/SendEmaiDAO.java
@@ -12,7 +12,6 @@
  * Nov 23, 2019			QuocLH				Create
  */
 
-
 package model.dao;
 
 import java.util.List;
@@ -27,61 +26,48 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import model.bean.ChuyenXe;
+import model.bean.MailConfig;
 import model.bean.SeatBooking;
 import model.bean.User;
 
-
 public class SendEmaiDAO {
-	
-	public static final String HOST_NAME = "smtp.gmail.com";
-	 
-    public static final int SSL_PORT = 465; // Port for SSL
- 
-    public static final int TSL_PORT = 587; // Port for TLS/STARTTLS
- 
-    public static final String APP_EMAIL = "fbus1912@gmail.com"; // your email
- 
-    public static final String APP_PASSWORD = "cap123456"; // your password
- 
+
 	public void SendMailCustomerDAO(ChuyenXe tripInfor, User user, List<SeatBooking> listSeat) {
 
 		// 1) get the session object
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", HOST_NAME);
-        props.put("mail.smtp.socketFactory.port", SSL_PORT);
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.port", SSL_PORT);
- 
-        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(APP_EMAIL, APP_PASSWORD);
-            }
-        });
- 
-        // 2) compose message
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(APP_EMAIL));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
- 
-            // 3) create HTML content
-            message.setSubject("[FastBus] Xác nhận thông tin vé xe ngay "+ tripInfor.getStartDate()+". ");
-            String htmlContent = 
-            		" <p>Thông tin các vé xe bạn đã đặt: <p>"
-            					+ " <p>Nhà xe: "+user.getName()+"</p>"
-            					+ " <p> Chuyến xe: "+tripInfor.getStartPlace()+" - "+tripInfor.getEndPlace()+"</p>"
-            					+ " <p>Ngày đi: "+tripInfor.getStartDate()+"</p>"
-            					+ " <p>Giờ đi: "+tripInfor.getStartTime()+"</p>"
-            					+ " <p>Ngày đi: "+tripInfor.getStartDate()+"</p>";
-            message.setContent(htmlContent, "UTF-8");
-             
-            // 4) send message
-            Transport.send(message);
-            
-            System.out.println("Message sent successfully");
-        } catch (MessagingException ex) {
-            ex.printStackTrace();
-        }
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.host", MailConfig.HOST_NAME);
+		props.put("mail.smtp.socketFactory.port", MailConfig.SSL_PORT);
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.port", MailConfig.SSL_PORT);
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(MailConfig.APP_EMAIL, MailConfig.APP_PASSWORD);
+			}
+		});
+		// 2) compose message
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(MailConfig.APP_EMAIL));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+
+			// 3) create HTML content
+			message.setSubject("[FastBus] Xác nhận thông tin vé xe ngay " + tripInfor.getStartDate() + ". ", "UTF-8");
+			String htmlContent = "<p> Xin chào "+user.getName()+"</p>"+
+					" <p>Thông tin các vé xe bạn đã đặt: <p>" + " <p>Nhà xe: " + tripInfor.getNameBusiness() + "</p>"
+					+ " <p> Chuyến xe: " + tripInfor.getStartPlace() + " - " + tripInfor.getEndPlace() + "</p>"
+					+ " <p>Ngày đi: " + tripInfor.getStartDate() + "</p>" + " <p>Giờ đi: " + tripInfor.getStartTime()
+					+ "</p>" + " <p>Ngày đi: " + tripInfor.getStartDate() + "</p>";
+			message.setContent(htmlContent, "text/html; charset=UTF-8");
+			// 4) send message
+			Transport.send(message);
+
+			System.out.println("Message sent successfully");
+		} catch (MessagingException ex) {
+			ex.printStackTrace();
+		}
 	}
+
 }
